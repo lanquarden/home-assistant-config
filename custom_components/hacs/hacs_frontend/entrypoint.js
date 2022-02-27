35af74c1b325772d7178565710071599366c4383
lanquarden <lanquarden@gmail.com>
2022-02-27 21:09:52 +0000

    Updated hacs

--- custom_components/hacs/hacs_frontend/entrypoint.js
+++ custom_components/hacs/hacs_frontend/entrypoint.js
@@ -1,9 +1,10 @@
 
 try {
-  new Function("import('/hacsfiles/frontend/main-8113c4f2.js')")();
+  new Function("import('/hacsfiles/frontend/main-def8a0ab.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/hacsfiles/frontend/main-8113c4f2.js';
+  el.src = '/hacsfiles/frontend/main-def8a0ab.js';
+  el.type = 'module';
   document.body.appendChild(el);
 }
   
